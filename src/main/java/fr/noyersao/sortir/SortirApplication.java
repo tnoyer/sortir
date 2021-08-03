f19ae6793c5f8c87ccf1154ce78cf5a8154d597f
Théophile <theophile.noyer@hotmail.fr>
2021-08-03 11:42:07 +0200

    first commit

---
+++ src/main/java/fr/noyersao/sortir/SortirApplication.java
@@ -0,0 +1,13 @@
+package fr.noyersao.sortir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/noyersao/sortir/SortirApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.noyersao.sortir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
